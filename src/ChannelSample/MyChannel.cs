47c9290c6a0f940f699329651a952efd2ee1b36a
fyzact <feyyazacet@gmail.com>
2022-08-11 00:48:05 +0100

    create project

---
+++ src/ChannelSample/MyChannel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+
+namespace ChannelSample
+{
+    public sealed class MyChannel<T>
+    {
+        ConcurrentQueue<T> _queue = new ConcurrentQueue<T>();
+        SemaphoreSlim _semaphore = new SemaphoreSlim(0);
+        public void Write(T item)
+        {
+            _queue.Enqueue(item);
+            _semaphore.Release();
+
+        }
+
+        public async Task<T> ReadAsync()
+        {
+            await _semaphore.WaitAsync().ConfigureAwait(false);
+            _queue.TryDequeue(out T item);
+            return item;
+        }
+    }
+}
