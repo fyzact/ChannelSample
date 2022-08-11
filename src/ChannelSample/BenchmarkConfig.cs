5209b1b4ffcc29f6675e57aad29f3b52cd0f34b8
fyzact <feyyazacet@gmail.com>
2022-08-11 01:05:16 +0100

    add benchmarknet config

---
+++ src/ChannelSample/BenchmarkConfig.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Analysers;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+
+namespace ChannelSample
+{
+    internal class BenchmarkConfig: ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            AddAnalyser(new BaselineCustomAnalyzer());
+        }
+    }
+}
